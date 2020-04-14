bc8bdb43ec6f8cfe76dfad1b7af9ee0f66347567
MaaarcosG <gut17909@uvg.edu.gt>
2020-04-13 18:13:00 -0600

    update Monitor

--- Monitores.c
+++ Monitores.c
@@ -13,25 +13,31 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-void *recurso(void* data);
 struct args {FILE* info;};
+void *recurso(void* data);
 
 /*Variables de la cantidad de thread*/
 #define amount_thread 10
 /*Variable de la cantidad de iteraciones*/
 #define amount_ite 4
 
-/*creamos el semaforo*/
-sem_t sem;
+/*recursos disponibles y recursos consumidos*/
+int available_resources = 50;
+int count = 40;
+
 /*recursos compartido*/
 int recurse = 0;
 
+/*Se crea el mutex y la variable condicion*/
+pthread_cond_t condition;
+pthread_mutex_t mutex;
 
+/*main*/
 int main(void){
 
 	/*Creamos y abrimos el archivo txt donde estara la bitacora*/
 	FILE *fichero;
-	fichero = fopen("BitacoraSemaforo.txt", "wt");
+	fichero = fopen("BitacoraMonitores.txt", "wt");
 
 	/*Envio de datos*/
 	struct args *archivo = (struct args *)malloc(sizeof(struct args));
@@ -45,9 +51,7 @@ int main(void){
 	
 	/*Creamos los thread*/
 	pthread_t thread[amount_thread];
-	/*Inicializamos el semaforo*/
-	sem_init(&sem, 0, 1); 
-
+	
 	/*Ciclo que creara los pthread conforme a la cantidad de pthread deseados
 	  el (void*)archivo manda el pthread creado hacia el archivo donde se escribira.
 	*/
@@ -59,40 +63,75 @@ int main(void){
 		pthread_join(thread[j], NULL);
 	}
 
-	//printf("valor del counter %d\n", recurse);
-
 	/*Se terminara el pograma y se cerrara el archivo*/
 	fprintf(fichero, "Fertig!\n");
 	fclose(fichero);
 	return 0;
 }
 
+/*decrease available resources by count resources */
+/*return 0 if sufficient resource available,  */
+/*otherwise return -1*/
+int decrease_count(int count, void *data){
+	if(available_resources < count){
+		/*bloquea y permite que otro proceso entre al monitor*/
+		fprintf(((struct args*)data)->info, "NUAY! Recursos actuales: %d\n", available_resources);
+		pthread_cond_wait(&condition, &mutex);
+	}
+
+	fprintf(((struct args*)data)->info, "Recursos suficientes disponibles, consumiendo...\n");
+	available_resources -= count;
+	sleep(1);
+	fprintf(((struct args*)data)->info, "Terminando decrease_count\n");
+	pthread_mutex_unlock(&mutex);
+}
+
+/*increase available resources by count*/
+int increase_count(int count, void *data){
+	pthread_mutex_lock(&mutex);
+
+	if(available_resources <= count){
+		fprintf(((struct args*)data)->info, "Avisando a la mara\n");
+		pthread_cond_signal(&condition);
+	}
+
+	available_resources += count;
+	return 0;
+} 
+
 void *recurso(void* data){
 	/*imprimimos cada pthread*/
 	fprintf(((struct args*)data)->info, "Iniciando Threads %ld\n", syscall(SYS_gettid));
-	fprintf(((struct args*)data)->info, "Esperando threads\n");
-	fprintf(((struct args*)data)->info, "%ld - Semaforo abierto con exito\n", syscall(SYS_gettid));
 
 	for(int k=0; k<amount_ite; k++){
 		fprintf(((struct args*)data)->info, "Iniciando iteracion%d\n", k+1);
-		/*bloqueara el siguiente hilo*/
-		sem_wait(&sem);
+		fprintf(((struct args*)data)->info, "Se consumiran %d recursos\n", count);
+
+		/*se bloquea el mutex*/
+		pthread_mutex_lock(&mutex);
+
 		/*Se usa el recurso*/
 		recurse +=1;
 		fprintf(((struct args*)data)->info, "%ld - (!) Recurso tomado\n", syscall(SYS_gettid));
 		fprintf(((struct args*)data)->info, "%ld - Buenos dias! Recurso usado\n", syscall(SYS_gettid));
-		/*Incrementamos unidad del semaforo*/
-		sem_post(&sem);
-		/*Del recurso sumado, le quitamos uno para simular la devolucion del recurso*/
-		recurse -=1;
-		sleep(0.7);
-		fprintf(((struct args*)data)->info, "%ld - Recurso devuelto :)\n", syscall(SYS_gettid));
+
+		fprintf(((struct args*)data)->info, "Iniciando deacrease\n");
+		fprintf(((struct args*)data)->info, "Mutex adquirido, entrando al monitor\n");
+
+		decrease_count(count, (void*)((struct args*)data));
+
+		fprintf(((struct args*)data)->info, "Iniciando deacrease\n");
+
+		increase_count(count, (void*)((struct args*)data));
+		fprintf(((struct args*)data)->info, "Mutex liberado\n");
+
+		/*se usa el siguiente monitor*/
+		pthread_mutex_unlock(&mutex);
+		fprintf(((struct args*)data)->info, "Terminando increase_count\n");
 		
 	}
 
 	/*Salida del thread*/
-	
-	fprintf(((struct args*)data)->info, "%ld - Semaforo muerto\n", syscall(SYS_gettid));
-	fprintf(((struct args*)data)->info, "Terminando thread %ld\n", syscall(SYS_gettid));
 	pthread_exit(0);
+
 }
